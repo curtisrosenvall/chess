e10573a59e7f38a1a55e7e5fd15173b9f50f1b38
boxtrain <curt.rosenvall11@gmail.com>
2024-05-27 14:29:50 -0600

    Added some comments to stay orginized

--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -1,3 +1,10 @@
 package model;
 
-public record UserData (String username, String password,String email){ }
+/**
+ * A record representing user data.
+ *
+ * @param username the username of the user
+ * @param password the password of the user
+ * @param email the email address of the user
+ */
+public record UserData(String username, String password, String email) { }
