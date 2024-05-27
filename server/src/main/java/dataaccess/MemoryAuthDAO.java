83ec8c19da2a92991da3d4362479591d74fa9e7c
boxtrain <curt.rosenvall11@gmail.com>
2024-05-27 14:37:00 -0600

    implemented MemoryAuthDAO

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,7 +1,44 @@
 package dataaccess;
 
+import java.util.ArrayList;
+import model.AuthData;
+import java.util.HashMap;
+import java.util.Map;
 
+public class MemoryAuthDAO implements AuthDAO {
 
-public class MemoryAuthDAO {
+    HashMap<String, AuthData> mapOfAuthData;
+
+    public MemoryAuthDAO() {
+        mapOfAuthData = new HashMap<>();
+    }
+
+    public void clear(){
+        mapOfAuthData.clear();
+    }
+
+    public void createAuth(String token, AuthData data){
+        mapOfAuthData.put(token, data);
+    }
+
+    public void deleteAuth(String token){
+        mapOfAuthData.remove(token);
+    }
+
+    public AuthData getAuth(String token){
+        return mapOfAuthData.get(token);
+    }
+
+    public int size(){
+        return mapOfAuthData.size();
+    }
+
+    public ArrayList<AuthData> getAllAuths() {
+        ArrayList<AuthData> allAuths = new ArrayList<>();
+        for(Map.Entry<String, AuthData> entry : mapOfAuthData.entrySet()) {
+            allAuths.add(entry.getValue());
+        }
+        return allAuths;
+    }
 
 }
