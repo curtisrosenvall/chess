eb68679398985f00448a808ed8b0387a7cedd09a
boxtrain <curt.rosenvall11@gmail.com>
2024-05-27 14:12:44 -0600

    setting up the AuthDAO and redoing the database

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,6 +1,16 @@
 package dataaccess;
 
-public class AuthDAO {
+import model.AuthData;
 
+import java.util.ArrayList;
+
+public interface AuthDAO {
+
+    void clear() throws DataAccessException;
+    void createAuth(String token, AuthData data) throws DataAccessException;
+    void deleteAuth(String token) throws DataAccessException;
+    AuthData getAuth(String token) throws DataAccessException;
+    int size();
+    ArrayList<AuthData> getAllAuths();
 
 }
