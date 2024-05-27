65fd51c3d0b71f680623eb9c8c05732815b3500b
boxtrain <curt.rosenvall11@gmail.com>
2024-05-27 15:06:10 -0600

    implemented the MemoryUserDAO

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,5 +1,4 @@
 package dataaccess;
-
 import java.util.ArrayList;
 import model.AuthData;
 import java.util.HashMap;
--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,4 +1,34 @@
 package dataaccess;
 
-public class MemoryUserDAO {
+import model.UserData;
+
+import java.util.HashMap;
+
+public class MemoryUserDAO implements UserDAO {
+    HashMap<String, UserData> mapOfUserData;
+
+    public MemoryUserDAO() {
+        mapOfUserData = new HashMap<>();
+    }
+
+    public void clear(){
+        mapOfUserData.clear();
+    }
+
+    public void createUser(String username, UserData authData){
+        mapOfUserData.put(username, authData);
+    }
+
+    public void deleteUser (String name){
+        mapOfUserData.remove(name);
+    }
+
+    public UserData getUser(String name){
+        return mapOfUserData.get(name);
+    }
+
+    public int size(){
+        return mapOfUserData.size();
+    }
+
 }
