80b3b51949c76081138eda87cc28be3b821a8241
boxtrain <curt.rosenvall11@gmail.com>
2024-05-28 11:18:02 -0600

    testing stuff within the server class

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,15 +1,26 @@
 package server;
-
+import dataaccess.Database;
+import handler.ClearAll;
+import handler.CreateGame;
+import handler.JoinGame;
 import spark.*;
 
 public class Server {
+
+    Database database;
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
 
+        database = new Database();
+
+
         // Register your endpoints and handle exceptions here.
+        Spark.post("/game", (req, res) -> (new CreateGame(database)).handle(req, res));
+        Spark.put("/game", (req, res) -> (new JoinGame(database)).handle(req, res));
+        Spark.delete("/db", (req, res) -> (new ClearAll(database)).handle(req, res));
 
         Spark.awaitInitialization();
         return Spark.port();
