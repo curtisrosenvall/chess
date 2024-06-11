db600dc11c18ba86f473fe66c9d7330cb1bbfe5c
boxtrain <curt.rosenvall11@gmail.com>
2024-06-11 13:07:59 -0600

    make register user method for server facade

--- client/src/main/java/Clienter2Sever/ServerFacade.java
+++ client/src/main/java/Clienter2Sever/ServerFacade.java
@@ -1,4 +1,23 @@
 package Clienter2Sever;
 
+import com.google.gson.Gson;
+import request.RegisterRequest;
+import result.RegisterResult;
+
+import java.io.InputStreamReader;
+
 public class ServerFacade {
+
+    ClientCommunicator clientCommunicator;
+
+    public ServerFacade(int port) {
+        clientCommunicator = new ClientCommunicator(port);
+    }
+
+    public RegisterResult registerUser(String username, String password, String email) {
+        RegisterRequest request = new RegisterRequest(username, password, email);
+        URLStrings clientStrings = new URLStrings("/user", "POST", "");
+        InputStreamReader reader = clientCommunicator.client2Server(request,clientStrings);
+        return new Gson().fromJson(reader, RegisterResult.class)
+    }
 }
