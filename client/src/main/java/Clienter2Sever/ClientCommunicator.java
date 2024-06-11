e693ea48e4b0f66747b9564a66a3bafb53c2158a
boxtrain <curt.rosenvall11@gmail.com>
2024-06-11 11:39:20 -0600

    working on making it easier to handle http requests

---
+++ client/src/main/java/Clienter2Sever/ClientCommunicator.java
@@ -0,0 +1,18 @@
+package Clienter2Sever;
+
+import request.ParentRequest;
+
+import java.io.InputStream;
+
+public class ClientCommunicator {
+
+    int port;
+
+    public ClientCommunicator(int port) {
+        this.port = port;
+    }
+
+    public InputStream client2Server(ParentRequest request, URLClientStrings clientStrings){
+        
+    }
+}
