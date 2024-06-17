a278eed7ed877b26e2c06e2b633a5f5769d839f9
boxtrain <curt.rosenvall11@gmail.com>
2024-06-17 12:19:22 -0600

    adding more messages and commands

---
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -0,0 +1,34 @@
+package websocket.commands;
+
+import chess.ChessMove;
+
+public class MakeMoveCommand extends UserGameCommand{
+
+    private Integer gameID;
+    private ChessMove move;
+
+
+    public MakeMoveCommand(String authToken, Integer gameID, ChessMove move) {
+        super(authToken);
+        this.commandType = CommandType.MAKE_MOVE;
+        this.gameID = gameID;
+        this.move = move;
+
+    }
+
+    public Integer getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(Integer gameID) {
+        this.gameID = gameID;
+    }
+
+    public ChessMove getMove() {
+        return move;
+    }
+
+    public void setMove(ChessMove move) {
+        this.move = move;
+    }
+}
