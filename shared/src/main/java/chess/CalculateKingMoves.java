608380aefb65f5bb93898fe251d18f334983347a
boxtrain <curt.rosenvall11@gmail.com>
2024-09-20 16:35:33 -0600

    kings tess pass

--- shared/src/main/java/chess/CalculateKingMoves.java
+++ shared/src/main/java/chess/CalculateKingMoves.java
@@ -3,6 +3,9 @@ package chess;
 import java.util.Collection;
 import java.util.ArrayList;
 
+/**
+ * Calculates valid moves for a King on a chessboard.
+ */
 public class CalculateKingMoves {
 
     Collection<ChessMove> validMovesCalculator;
@@ -12,47 +15,36 @@ public class CalculateKingMoves {
         validMovesCalculator = new ArrayList<>();
         ValidMovesCalculator validMove = new ValidMovesCalculator();
 
-        //Up
-        ChessPosition endPosition = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn());
-        if(validMove.isInBoard(endPosition))
-            validMove.movePiece(validMovesCalculator, board, startPosition, endPosition);
+        // Up, Up-Right, Right, Down-Right, Down, Down-Left, Left, Up-Left
+        int[][] directions = {
+                {1, 0},   // Up
+                {1, 1},   // Up-Right
+                {0, 1},   // Right
+                {-1, 1},  // Down-Right
+                {-1, 0},  // Down
+                {-1, -1}, // Down-Left
+                {0, -1},  // Left
+                {1, -1}   // Up-Left
+        };
 
-        //Up and to the right
-        endPosition = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn() + 1);
-        if(validMove.isInBoard(endPosition))
-            validMove.movePiece(validMovesCalculator, board, startPosition, endPosition);
+        // Iterate through each direction and calculate valid moves
+        for (int[] direction : directions) {
+            ChessPosition endPosition = new ChessPosition(
+                    startPosition.getRow() + direction[0],
+                    startPosition.getColumn() + direction[1]
+            );
 
-        //Right
-        endPosition = new ChessPosition(startPosition.getRow(), startPosition.getColumn() + 1);
-        if(validMove.isInBoard(endPosition))
-            validMove.movePiece(validMovesCalculator, board, startPosition, endPosition);
-
-        //Down and Right
-        endPosition = new ChessPosition(startPosition.getRow() - 1, startPosition.getColumn() + 1);
-        if(validMove.isInBoard(endPosition))
-            validMove.movePiece(validMovesCalculator, board, startPosition, endPosition);
-
-        //Down
-        endPosition = new ChessPosition(startPosition.getRow() - 1, startPosition.getColumn());
-        if(validMove.isInBoard(endPosition))
-            validMove.movePiece(validMovesCalculator, board, startPosition, endPosition);
-
-        //Down and Left
-        endPosition = new ChessPosition(startPosition.getRow() - 1, startPosition.getColumn() - 1);
-        if(validMove.isInBoard(endPosition))
-            validMove.movePiece(validMovesCalculator, board, startPosition, endPosition);
-
-        //Left
-        endPosition = new ChessPosition(startPosition.getRow(), startPosition.getColumn() - 1);
-        if(validMove.isInBoard(endPosition))
-            validMove.movePiece(validMovesCalculator, board, startPosition, endPosition);
-
-        //Up and Left
-        endPosition = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn() - 1);
-        if(validMove.isInBoard(endPosition))
-            validMove.movePiece(validMovesCalculator, board, startPosition, endPosition);
+            if (validMove.isInBoard(endPosition)) {
+                validMove.movePiece(validMovesCalculator, board, startPosition, endPosition);
+            }
+        }
     }
 
+    /**
+     * Retrieves the collection of valid King moves.
+     *
+     * @return A collection of valid ChessMove objects for the King.
+     */
     public Collection<ChessMove> getKingMoves() {
         return validMovesCalculator;
     }
