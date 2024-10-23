f5a86de226574b4303d9bb51e25201ed7187f25c
boxtrain <curt.rosenvall11@gmail.com>
2024-10-23 13:03:33 -0600

    trying to figure out why register doesnt work

--- server/src/main/java/handlers/Register.java
+++ server/src/main/java/handlers/Register.java
@@ -1,9 +1,42 @@
 package handlers;
 
+import dataaccess.DataAccessException;
+import dataaccess.Database;
+import request.RegisterReq;
+import result.RegisterRes;
+import service.UserService;
+import spark.Request;
+import spark.Response;
+import spark.Route;
 
+public class Register implements Route {
 
+    Database database;
+    MethodHandlers methodHandlers;
 
-public class Register  {
+    public Register(Database database) {
+        this.database = database;
+        methodHandlers = new MethodHandlers();
+    }
 
-
+    @Override
+    public Object handle(Request request, Response response) {
+        RegisterReq registerRequest;
+        try {
+            registerRequest = (RegisterReq) methodHandlers.getBody(request, "RegisterRequest");
+            methodHandlers.isNullString(registerRequest.getUsername());
+            methodHandlers.isNullString(registerRequest.getPassword());
+            methodHandlers.isNullString(registerRequest.getEmail());
+        } catch(DataAccessException ex) {
+            return methodHandlers.getResponse(response,400, new RegisterRes(null, "Error: Bad Request", null, null));
+        }
+        UserService register = new UserService(database);
+        RegisterRes registerResult = register.createUser(registerRequest);
+        if(registerResult.isSuccess()) {
+            return methodHandlers.getResponse(response, 200, registerResult);
+        } else if(registerResult.getMessage().contains("already taken")) {
+            return methodHandlers.getResponse(response, 403, registerResult);
+        } else
+            return methodHandlers.getResponse(response, 500, registerResult);
+    }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,15 +1,21 @@
 package server;
-
+import dataaccess.*;
+import handlers.Register;
 import spark.*;
 
 public class Server {
+
+    Database database;
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
 
+        database = new Database();
+
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", (req, res) -> (new Register(database)).handle(req,res));
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
