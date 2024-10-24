60e6140cd459a6f82d3f3a5a4f57fcc12e3de776
boxtrain <curt.rosenvall11@gmail.com>
2024-10-24 10:29:48 -0600

    should have all unit tests done now

---
+++ server/src/test/java/service/ClearGameServiceTest.java
@@ -0,0 +1,62 @@
+package service;
+
+import dataaccess.*;
+import org.junit.jupiter.api.*;
+import result.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for ClearGameService.
+ */
+class ClearGameServiceTest extends ParentTest {
+
+    private Database db;
+    private ClearGameService clear;
+    private ClearRes result;
+
+    @BeforeEach
+    public void setUp() {
+        db = new Database();
+        clear = new ClearGameService(db);
+    }
+
+    @Test
+    public void clearAllWithUser() {
+        try {
+            db.createUser("Test", "1234", "test@gmail.com");
+            result = clear.deleteAll();
+            assertTrue(result.isSuccess(), result.getMessage());
+        } catch (DataAccessException ex) {
+            fail("Unexpected DataAccessException: " + ex.getMessage());
+        }
+    }
+
+    @Test
+    public void clearAllWithGame() {
+        try {
+            db.createGame("Test");
+            result = clear.deleteAll();
+            assertTrue(result.isSuccess(), result.getMessage());
+        } catch (DataAccessException ex) {
+            fail("Unexpected DataAccessException: " + ex.getMessage());
+        }
+    }
+
+    @Test
+    public void clearAllWithAuth() {
+        try {
+            db.createAuth("TOKEN", "Test");
+            result = clear.deleteAll();
+            assertTrue(result.isSuccess(), result.getMessage());
+        } catch (DataAccessException ex) {
+            fail("Unexpected DataAccessException: " + ex.getMessage());
+        }
+    }
+
+    @Test
+    public void clearAllWhenEmpty() {
+        result = clear.deleteAll();
+        assertTrue(result.isSuccess(), result.getMessage());
+    }
+}
