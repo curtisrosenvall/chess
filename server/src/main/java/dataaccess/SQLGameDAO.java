854bef3c04160ffd6881865a636e6dc5bac8137a
boxtrain <curt.rosenvall11@gmail.com>
2024-11-01 11:19:31 -0600

    basic structure for SQLGameDAO set up

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,8 +1,8 @@
 package dataaccess;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import models.*;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class SQLGameDAO implements GameDAO {
 
@@ -20,5 +20,25 @@ public class SQLGameDAO implements GameDAO {
     @Override
     public void createGame(String name) throws DataAccessException {
         throw new DataAccessException("Error: ");
+    }
+
+    @Override
+    public GameData getGame(int id) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<GameData> listGames(){
+        return null;
+    }
+
+    @Override
+    public void updateGame(int id, GameData game){
+
+    }
+
+    @Override
+    public int size(){
+        return 1;
     }
 }
