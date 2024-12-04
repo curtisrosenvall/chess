f5113a0ad79d755748f1b9634c7bdeb8c0e1e226
boxtrain <curt.rosenvall11@gmail.com>
2024-12-04 13:27:14 -0700

    im stuck

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,13 +1,15 @@
 package server;
+import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPiece;
 import com.google.gson.Gson;
 import dataaccess.*;
 import handlers.*;
 import models.AuthData;
 import models.GameData;
-import org.eclipse.jetty.websocket.api.*;
+import org.eclipse.jetty.websocket.api.RemoteEndpoint;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
-import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import spark.*;
 import websocket.commands.*;
 import websocket.messages.ErrorMessage;
@@ -64,15 +66,15 @@ public class Server {
                 }
                 case MAKE_MOVE -> {
                     MakeMove moveCommand = new Gson().fromJson(message, MakeMove.class);
-                    // Implement moveCommand handling here
+                    makeMove(session, username, moveCommand);
                 }
                 case LEAVE -> {
                     Leave leaveCommand = new Gson().fromJson(message, Leave.class);
-                    // Implement leaveCommand handling here
+                    leaveGame(session, username, leaveCommand);
                 }
                 case RESIGN -> {
                     Resign resignCommand = new Gson().fromJson(message, Resign.class);
-                    // Implement resign handling here
+                    resign(session, username, resignCommand);
                 }
             }
         } catch (Exception ex) {
@@ -92,16 +94,16 @@ public class Server {
 
     public void notifySessions(ArrayList<Session> sessionList, Session rootUser, ServerMessage message, String notifyWay) {
         switch (notifyWay) {
-            case "ROOT" -> sendMessage(rootUser.getRemote(), message);
+            case "ROOT" -> sendMessage(rootUser, message);
             case "NOT_ROOT" -> {
                 for (Session session : sessionList) {
                     if (!session.equals(rootUser))
-                        sendMessage(session.getRemote(), message);
+                        sendMessage(session, message);
                 }
             }
             case "ALL" -> {
                 for (Session session : sessionList) {
-                    sendMessage(session.getRemote(), message);
+                    sendMessage(session, message);
                 }
             }
         }
@@ -140,5 +142,137 @@ public class Server {
             System.out.println("Error trying to get the database");
         }
     }
+
+    public void makeMove(Session session, String username, MakeMove command) {
+        try {
+            ChessMove move = command.getMove();
+            String stringMove = getStringVersion(move);
+            if (stringMove == null) {
+                sendMessage(session, new ErrorMessage("Invalid move"));
+                return;
+            }
+            GameData game = database.getGame(command.getGameID());
+            ChessGame.TeamColor color = game.game().getTeamTurn();
+            if (!username.equals(game.whiteUsername()) && !username.equals(game.blackUsername())) {
+                sendMessage(session, new ErrorMessage("You cannot move pieces as the observer"));
+                return;
+            }
+            ChessGame.TeamColor playerColor = (game.whiteUsername().equals(username))
+                    ? ChessGame.TeamColor.WHITE
+                    : ChessGame.TeamColor.BLACK;
+            if (game.game().getBoard().getPiece(move.getStartPosition()).getTeamColor() != playerColor) {
+                sendMessage(session, new ErrorMessage("You cannot move the other team's pieces"));
+                return;
+            }
+            game.game().makeMove(move);
+            database.updateGame(game);
+
+            ArrayList<Session> sessionList = database.getSessionList(command.getGameID());
+
+            notifySessions(sessionList, session, new LoadGame(game), "ALL");
+            notifySessions(sessionList, session, new Notification(username + " has made a move: " + stringMove), "NOT_ROOT");
+            if (game.game().isInCheckmate(game.game().getTeamTurn())) {
+                notifySessions(sessionList, session,
+                        new Notification(username + " has Checkmated " + ((color == ChessGame.TeamColor.WHITE)
+                                ? game.blackUsername()
+                                : game.whiteUsername())),
+                        "ALL");
+            } else if (game.game().isInStalemate(game.game().getTeamTurn())) {
+                notifySessions(sessionList, session,
+                        new Notification(((color == ChessGame.TeamColor.WHITE)
+                                ? game.blackUsername()
+                                : game.whiteUsername()) + " is in Stalemate"),
+                        "ALL");
+            } else if (game.game().isInCheck(game.game().getTeamTurn())) {
+                notifySessions(sessionList, session,
+                        new Notification(username + " has Checked " + ((color == ChessGame.TeamColor.WHITE)
+                                ? game.blackUsername()
+                                : game.whiteUsername())),
+                        "ALL");
+            }
+        } catch (Exception ex) {
+            sendMessage(session, new ErrorMessage("Error: " + ex.getMessage()));
+        }
+    }
+
+    public void leaveGame(Session session, String username, Leave command) {
+        database.removeSession(command.getGameID(), session);
+        try {
+            GameData game = database.getGame(command.getGameID());
+            GameData newGame;
+            if((game.blackUsername() != null) && game.blackUsername().equals(username))
+                newGame = new GameData(game.gameID(), game.whiteUsername(), null, game.gameName(), game.game());
+            else if((game.whiteUsername() != null) && game.whiteUsername().equals(username))
+                newGame = new GameData(game.gameID(), null, game.blackUsername(), game.gameName(), game.game());
+            else
+                newGame = game;
+            database.updateGame(newGame);
+            notifySessions(database.getSessionList(command.getGameID()), session, new Notification(username + " has left the game"), "NOT_ROOT");
+        } catch(Exception ex) {
+            System.out.println("NOT IN GAME!!!");
+        }
+    }
+
+    public void resign(Session session, String username, Resign command) {
+        try {
+            GameData game = database.getGame(command.getGameID());
+
+            if (!username.equals(game.whiteUsername()) && !username.equals(game.blackUsername())) {
+                sendMessage(session, new ErrorMessage("You can't resign as the observer"));
+                return;
+            }
+
+            if (game.game().isGameOver()) {
+                sendMessage(session, new ErrorMessage("The game is over, no more moves or resignations can be made"));
+                return;
+            }
+
+            ChessGame resignGame = game.game();
+            resignGame.setGameOver(true);
+            GameData newGame = new GameData(game.gameID(), game.whiteUsername(), game.blackUsername(), game.gameName(), resignGame);
+            database.updateGame(newGame);
+
+            ArrayList<Session> sessionList = database.getSessionList(command.getGameID());
+
+            notifySessions(sessionList, session, new Notification(username + " has resigned the game"), "ALL");
+        } catch (Exception ex) {
+            System.out.println("Failed to resign");
+        }
+    }
+
+    public String getStringVersion(ChessMove move) {
+
+        String startPos = intToChar(move.getStartPosition().getColumn()) + String.valueOf(move.getStartPosition().getRow());
+        String endPos = intToChar(move.getEndPosition().getColumn()) + String.valueOf(move.getEndPosition().getRow());
+        String promotion = pieceToString(move.getPromotionPiece());
+
+        return startPos + " to " + endPos + ((promotion == null) ? "" : promotion);
+    }
+
+    public Character intToChar(int col) {
+        switch(col) {
+            case 1 -> { return 'a'; }
+            case 2 -> { return 'b'; }
+            case 3 -> { return 'c'; }
+            case 4 -> { return 'd'; }
+            case 5 -> { return 'e'; }
+            case 6 -> { return 'f'; }
+            case 7 -> { return 'g'; }
+            case 8 -> { return 'h'; }
+            default -> { return null; }
+        }
+    }
+
+    public String pieceToString(ChessPiece.PieceType piece) {
+        switch(piece) {
+            case null -> { return null; }
+            case QUEEN -> { return "promoting to a Queen"; }
+            case ROOK -> { return "promoting to a Rook"; }
+            case BISHOP -> { return "promoting to a Bishop"; }
+            case KNIGHT -> { return "promoting to a Knight"; }
+            default -> { return null; }
+        }
+    }
+
 
 }
